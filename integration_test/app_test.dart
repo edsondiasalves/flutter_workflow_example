9b473f037c14fd2b46043ed5a5d21de0ec56c524
= <edsondiasalves@hotmail.com>
2022-05-30 23:15:38 +0100

    Adding screenshot to integration tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import '../lib/main.dart' as app;
 
 void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding();
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter', (tester) async {
       app.main();
@@ -12,6 +16,7 @@ void main() {
 
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
+      await takeScreenshot(tester, binding, 'shot-1');
 
       // Finds the floating action button to tap on.
       final Finder fab = find.byTooltip('Increment');
@@ -21,9 +26,23 @@ void main() {
 
       // Trigger a frame.
       await tester.pumpAndSettle();
+      await takeScreenshot(tester, binding, 'shot-2');
 
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
     });
   });
+}
+
+takeScreenshot(tester, binding, name) async {
+  if (Platform.isAndroid) {
+    try {
+      await binding.convertFlutterSurfaceToImage();
+    } catch (e) {
+      print("TakeScreenshot exception $e");
+    }
+    await tester.pumpAndSettle();
+  }
+
+  await binding.takeScreenshot(name);
 }
